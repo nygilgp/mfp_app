ba687f2d0d9d0fb9fa61516e0c204e9f199f4d67
Nygil Pereira <nygilgp@gmail.com>
2021-10-14 13:37:15 +0530

    Redering marketing app mount inside container

--- packages/container/src/App.js
+++ packages/container/src/App.js
@@ -1,8 +1,12 @@
 import React from 'react';
-import { mount } from 'marketing/MarketingApp';
-
-console.log(mount);
+import MarketingApp from './components/MarketingApp';
 
 export default () => {
-  return <h1>Container...</h1>;
+  return (
+    <div>
+      <h1>Container...</h1>
+      <hr />
+      <MarketingApp />
+    </div>
+  );
 };
