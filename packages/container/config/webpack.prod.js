c8c0270088bf85dba0992b358ba0e7352c8d85aa
Nygil Pereira <nygilgp@gmail.com>
2021-10-17 16:52:48 +0530

    typo fix in path

--- packages/container/config/webpack.prod.js
+++ packages/container/config/webpack.prod.js
@@ -15,7 +15,7 @@ const prodConfig = {
     new ModuleFederationPlugin({
       name: 'container',
       remotes: {
-        marketing: `marketing@${domain}/maketing/latest/remoteEntry.js`,
+        marketing: `marketing@${domain}/marketing/latest/remoteEntry.js`,
       },
       shared: packageJason.dependencies,
     }),
