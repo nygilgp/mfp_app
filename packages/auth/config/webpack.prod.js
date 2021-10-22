9a83b81315112465d3b04afe19a1d39a7991f63a
Nygil Pereira <nygilgp@gmail.com>
2021-10-22 18:57:46 +0530

    Auth module added, webpack config updated for public path

--- packages/auth/config/webpack.prod.js
+++ packages/auth/config/webpack.prod.js
@@ -1,28 +1,27 @@
 const { merge } = require('webpack-merge');
 const ModuleFederationPlugin = require('webpack/lib/container/ModuleFederationPlugin');
+
 const commonConfig = require('./webpack.common');
 const packageJason = require('../package.json');
 
-const devConfig = {
-  mode: 'development',
+const domain = process.env.PRODUCTION_DOMAIN;
+
+const prodConfig = {
+  mode: 'production',
   output: {
-    publicPath: 'http://localhost:8080/',
-  },
-  devServer: {
-    port: 8080,
-    historyApiFallback: {
-      index: '/index.html',
-    },
+    filename: '[name].[contenthash].js',
+    publicPath: '/auth/latest/',
   },
   plugins: [
     new ModuleFederationPlugin({
-      name: 'container',
-      remotes: {
-        marketing: 'marketing@http://localhost:8081/remoteEntry.js',
+      name: 'auth',
+      filename: 'remoteEntry.js',
+      exposes: {
+        './AuthApp': './src/bootstrap',
       },
       shared: packageJason.dependencies,
     }),
   ],
 };
 
-module.exports = merge(commonConfig, devConfig);
+module.exports = merge(commonConfig, prodConfig);
